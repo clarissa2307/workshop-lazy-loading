3aaaaa0604ecdfcada888f34a9589052d9fa3ba8
clarissa2307 <clarissa2307@users.noreply.github.com>
2022-02-03 17:38:32 +0000

    deploy: 82bf99dca0671c07d78f9c2e8f6e65093065110b

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -4,17 +4,24 @@ const  isIntersecting  = (entry) => {
     return entry.isIntersecting;
 };
 
-const   accion = (entry) => {
-    const nodo = entry.target;
-    console.log("holis");
+const   loadImage = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const  url = imagen.dataset.src
+
+    // carga  la  imagen 
+    imagen.src = url;
+
+
+    //console.log(nodo.nodeName);
 
     // desregistra  las imagenes
 
-    observer.unobserve(nodo);
+    observer.unobserve(container);
 };
 
 const observer = new  IntersectionObserver((entries) =>  {
-    entries.filter(isIntersecting).forEach(accion);
+    entries.filter(isIntersecting).forEach(loadImage);
 
 });
 export const  registerImage  =  (imagen) => {
