f7ade2d9faceae0da1b6aa3e4de239df39a14e29
clarissa2307 <clarissa2307@users.noreply.github.com>
2022-02-03 05:29:18 +0000

    deploy: fdedc79192fc96cf9a97102bfa05096455d10a94

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,38 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//crear imagen 
+//agregar #imagen 
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement('div')
+    container.className = "p-4";
+
+    const imagen = document.createElement("img"); 
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.src =`https://source.unsplash.com/random/${random()}`; 
+
+    container.appendChild(imagen);
+    return container;
+
+    
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+} 
+addButton.addEventListener("click", addImage);
+
+
+
